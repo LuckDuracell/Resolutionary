b56bf7a7fcfb85ba077ab4773a6f6b54ff4cd3ec
LuckDev <lukedru@yahoo.com>
2022-12-11 02:54:38 -0800

    Initial Commit

---
+++ Resolutionary/ResolutionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResolutionaryApp.swift
+//  Resolutionary
+//
+//  Created by Luke Drushell on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ResolutionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
