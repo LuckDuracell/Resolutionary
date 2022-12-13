cbeac8e3742b794a0a188a9a14243ad32a1a6715
LuckDev <lukedru@yahoo.com>
2022-12-13 15:42:32 -0800

    i hate lists

--- Resolutionary/Main Items/GoalList.swift
+++ Resolutionary/Main Items/GoalList.swift
@@ -13,41 +13,83 @@ struct GoalList: View {
     @Binding var completed: [String]
     
     func deleteRes(_ index: Int) {
-        resolutions.remove(at: index)
-        completed.remove(at: index)
+        withAnimation {
+            resolutions.remove(at: index)
+            completed.remove(at: index)
+        }
         UserDefaults.standard.set(resolutions, forKey: "resolutions")
         UserDefaults.standard.set(completed, forKey: "completed")
     }
     
+    let screenWidth = UIScreen.main.bounds.width * 0.95
+    
+    @State var rowHeight: CGFloat = 45
+    
     var body: some View {
         VStack {
-            GroupBox("Your Goals:", content: {
-                List(resolutions.indices, id: \.self, rowContent: { index in
-                    Goal(resolutions: $resolutions, completed: $completed, index: index)
-                        .swipeActions(edge: .trailing, allowsFullSwipe: false) {
-                            Button {
-                                deleteRes(index)
-                            } label: {
-                                Text("Delete")
-                            } .tint(.red)
+            HStack {
+                Text("Your Goals:")
+                    .bold()
+                    .padding(.leading, 5)
+                Spacer()
+            }
+            //Divider()
+            List(resolutions.indices, id: \.self, rowContent: { index in
+                GeometryReader { (geometry) in
+                    HStack {
+                        TextField("Goal \(index + 1)", text: $resolutions[index])
+                            .onChange(of: resolutions[index], perform: { _ in
+                                UserDefaults.standard.set(resolutions, forKey: "resolutions")
+                            })
+                        Button {
+                            if completed[index] == "1" { completed[index] = "0" } else { completed[index] = "1" }
+                        } label: {
+                            Image(systemName: completed[index] == "1" ? "largecircle.fill.circle" : "circle")
+                                .foregroundColor(.accentColor)
                         }
-                })
-                HStack(alignment: .center) {
-                    Button {
-                        withAnimation {
-                            completed.append("0")
-                            resolutions.append("")
-                            UserDefaults.standard.set(resolutions, forKey: "resolutions")
+                        .onChange(of: completed[index], perform: { _ in
                             UserDefaults.standard.set(completed, forKey: "completed")
+                        })
+                        .padding(.top, 3)
+                    }
+                    .swipeActions(edge: .trailing, allowsFullSwipe: false) {
+                        Button {
+                            deleteRes(index)
+                        } label: {
+                            Text("Delete")
+                        } .tint(.red)
+                    }
+                    .listRowBackground(Color.clear)
+                    .onAppear(perform: {
+                        withAnimation {
+                            rowHeight = geometry.size.height
                         }
-                    } label: {
-                        Image(systemName: "plus.circle.fill")
-                            .foregroundStyle(.white, Color.accentColor)
-                            .font(.title)
-                    } .padding(5)
+                    })
                 }
-            })
-        }
+            }) .scrollContentBackground(.hidden)
+                .frame(width: screenWidth, height: rowHeight * CGFloat(resolutions.count) + 130, alignment: .center)
+                .edgesIgnoringSafeArea(.all)
+                .padding(.horizontal, -100)
+                .padding(.vertical, -40)
+            Divider()
+            HStack(alignment: .center) {
+                Button {
+                    withAnimation {
+                        completed.append("0")
+                        resolutions.append("")
+                        UserDefaults.standard.set(resolutions, forKey: "resolutions")
+                        UserDefaults.standard.set(completed, forKey: "completed")
+                    }
+                } label: {
+                    Image(systemName: "plus.circle.fill")
+                        .foregroundStyle(.white, Color.accentColor)
+                        .font(.title)
+                } .padding(3)
+            }
+        } .padding()
+            .background(.regularMaterial)
+            .cornerRadius(15)
+            .padding()
     }
 }
 
